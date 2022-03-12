47c38acda3989fd3ab80ecbdf54e1478dda1a33f
Asadbek <81373435+asadbek2021@users.noreply.github.com>
2022-03-12 22:33:04 +0500

    feat: add guard

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const statisticRouter = require('./resources/statistic/statistic.router');
 const categoryRouter = require('./resources/category/category.router');
 const authRouter = require('./resources/auth/auth.router');
 const { jwtCallback } = require('./resources/auth/auth.repository');
+const addGuard = require('./tools/guards');
 
 const auth = passport.authenticate('jwt', { session: false });
 const hbs = exhbs.create({
@@ -42,10 +43,10 @@ const opts = {
 };
 passport.use(new Strategy(opts, jwtCallback));
 
-app.use('/user', auth, userRouter);
+app.use('/user', auth, addGuard, userRouter);
 app.use('/transaction', auth, transactionRouter);
 app.use('/account', auth, accountRouter);
-app.use('/statistic', auth, statisticRouter);
+app.use('/statistic', auth, addGuard, statisticRouter);
 app.use('/category', auth, categoryRouter);
 app.use('/auth', authRouter);
 app.use('/', require('./resources/home.router'));
---
+++ src/tools/guards.js
@@ -0,0 +1,11 @@
+const db = require('../loader/dbconnect');
+
+const addGuard = function guard(req, res, next) {
+  const user = db.getUserByEmail(req.user.email);
+  if (user?.role === 'admin') {
+    return next();
+  }
+  return res.json({ message: 'You are not allowed' });
+};
+
+module.exports = addGuard;
