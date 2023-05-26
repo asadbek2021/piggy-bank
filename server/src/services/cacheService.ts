9c581c0bd0f9af2c07a697474b529291e8c20446
Asadbek Raimov <asadbek_raimov@epam.com>
2023-05-26 10:35:58 +0500

    fix(cache): return mongodb documents instead of plain objects

--- server/src/services/cacheService.ts
+++ server/src/services/cacheService.ts
@@ -9,7 +9,11 @@ mongoose.Query.prototype.exec = async function() {
     const key = JSON.stringify({...this.getQuery(), collection: this.model.collection.name});
     const cached = await client.get(key);
     if(cached){
-        return JSON.parse(cached);
+        const doc = JSON.parse(cached);
+
+        return Array.isArray(doc) 
+            ? doc.map(d => new this.model(d)) 
+            : new this.model(doc);
     }
     const result = await originalExec.apply(this, arguments as any);
     await client.set(key, JSON.stringify(result));
